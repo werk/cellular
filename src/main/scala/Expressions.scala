a627fbd85999c7f7e6c5f1b750668ac11209910d
ahnfelt <joakim.ahnfelt@gmail.com>
2020-08-18 00:23:36 +0200

    Experiments with translating Reactions

--- src/main/scala/Expressions.scala
+++ src/main/scala/Expressions.scala
@@ -7,6 +7,7 @@ object Expressions {
         def go(expression : Expression) = translate(expression, parenthesis = true)
         expression match {
             case Language.EIs(left, kind) =>
+                // #define is_heat(x) (get_heat(x) == 4294967295)
                 "is_" + kind + "(" + go(left) + ")"
             case Language.EField(left, kind) =>
                 "get_" + kind + "(" + go(left) + ")"
