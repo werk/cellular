dff1a2d9d6f779f7c027da862ef42a25bd201441
Michael Werk Ravnsmed <mfwerk@gmail.com>
2020-09-16 22:44:46 +0200

    more glsl generation

--- src/main/scala/language/Compile.scala
+++ src/main/scala/language/Compile.scala
@@ -22,6 +22,7 @@ object Compile {
             vec4ToInt,
             getEncodeFunction(cellTypeDeclarations.map(_.name), traitDeclarations.map(_.name)),
             getDecodeFunction(cellTypeDeclarations.map(_.name), traitDeclarations.map(_.name)),
+            lookupMaterialFunction,
             blocks(ruleFunctions),
             makeMain(blocks(ruleUsages))
         )
@@ -56,6 +57,13 @@ object Compile {
     val vec4ToInt : String = lines(
         "uint vec4ToInt(vec4 pixel) {",
         "    return uint(pixel.x) + 256 * (uint(pixel.y) + 256 * (uint(pixel.z) + 256 * uint(pixel.w)));",
+        "}",
+    )
+
+    val lookupMaterialFunction : String = lines(
+        "Material lookupMaterial(vec2 offset) {",
+        "    vec4 color = texture2D(state, offset / scale);",
+        "    return decode(color);",
         "}",
     )
 
@@ -171,21 +179,45 @@ object Compile {
             s"bool did_${r.name} = false;"
         )
         val groupCondition = Expressions.translate(g.condition, parenthesis = false)
-        val ruleCalls = g.reactions.flatMap{r => List(
-            s"if($groupCondition) {",
-            s"    did_${r.name} = did_${r.name} || rule_${r.name}(null); // TODO",
-            s"    did_${g.name} = did_${g.name} || did_${r.name}(null); // TODO",
-            s"}"
-        )}
+        val ruleCalls = g.reactions.flatMap{r =>
+            val peekArguments = r.constraints.map(Usages.peeks).fold(Set()) { _ ++ _ }.toList.sorted.
+                map((Usages.peek _).tupled)
+
+            List(
+                s"if($groupCondition) {",
+                s"    did_${r.name} = did_${r.name} || rule_${r.name}($peekArguments);",
+                s"    did_${g.name} = did_${g.name} || did_${r.name};",
+                s"}"
+            )
+        }
 
         (ruleFunctions, lines(didGroup :: didReactions ++ ruleCalls))
     }
 
     def makeMain(ruleUsages : String) : String = lines(
         "void main() {",
-        "    // read pp_0_0 etc.",
+        "    vec2 position = gl_FragCoord.xy - 0.5;",
+        "    vec2 offset = mod(float(step), 2.0) == 0.0 ? vec2(-1.0, -1.0) : vec2( 0.0,  0.0);",
+        "    vec2 bottomLeft = floor((position + offset) * 0.5) * 2.0 - offset;",
+        "",
+        "    // Read and parse relevant pixels",
+        "    Material pp_0_0 = lookupMaterial(bottomLeft + vec2(0.0, 0.0));",
+        "    Material pp_0_1 = lookupMaterial(bottomLeft + vec2(0.0, 1.0));",
+        "    Material pp_1_0 = lookupMaterial(bottomLeft + vec2(0.1, 0.0));",
+        "    Material pp_1_1 = lookupMaterial(bottomLeft + vec2(0.1, 1.0));",
+        "    Material pm_0_1 = lookupMaterial(bottomLeft + vec2(0.0, -1.0));",
+        "    Material pm_1_1 = lookupMaterial(bottomLeft + vec2(0.1, -1.0));",
+        "",
+        "    // Rules",
         indent(ruleUsages),
-        "    // write own pixel, e.g. pp_0_1.!",
+        "",
+        "   // Write and encode own material",
+        "   Material target = null;",
+        "   if(position == bottomLeft + vec2(0.0, 0.0)) target = pp_0_0;",
+        "   else if(position == bottomLeft + vec2(0.0, 0.1)) target = pp_0_1;",
+        "   else if(position == bottomLeft + vec2(1.0, 0.0)) target = pp_1_0;",
+        "   else if(position == bottomLeft + vec2(1.0, 0.1)) target = pp_1_1;",
+        "   gl_FragColor = encode(target);",
         "}",
     )
 
