e9c93b4bf18acac473011a1d9520b9383526e1e5
ahnfelt <joakim.ahnfelt@gmail.com>
2020-11-25 22:31:16 +0100

    Experiments with new minimal core language

---
+++ src/main/scala/cellular/mini/Syntax.scala
@@ -0,0 +1,31 @@
+package cellular.mini
+
+sealed trait Type
+case class TIntersection(type1: Type, type2: Type) extends Type
+case class TUnion(type1: Type, type2: Type) extends Type
+case class TProperty(property: String) extends Type
+case class TForget(property: String, value: Value) extends Type
+
+trait Pattern
+case class PVariable(name: Option[String]) extends Pattern
+case class PProperty(pattern: Pattern, property: String, value: Option[Value]) extends Pattern
+
+trait Expression
+case class EVariable(name: String) extends Expression
+case class EMatch(expression: Expression, matchCases: List[MatchCase]) extends Expression
+case class EProperty(expression: Expression, value: Expression) extends Expression
+case class EMaterial(material: String) extends Expression
+
+case class Value(material: String, properties: List[PropertyValue])
+case class PropertyValue(property: String, value: Value)
+case class MatchCase(pattern: Pattern, body: Expression)
+
+trait Definition
+case class DProperty(name: String, valueType: Option[Type]) extends Definition
+case class DMaterial(name: String, properties: List[String]) extends Definition
+
+case class TypeContext(
+    properties: Map[String, Option[Type]],
+    materials: Map[String, List[String]],
+    variables: Map[String, Type]
+)
---
+++ src/main/scala/cellular/mini/TypeChecker.scala
@@ -0,0 +1,57 @@
+package cellular.mini
+
+object TypeChecker {
+
+    def materials(context: TypeContext, type0: Type) : Set[String] = type0 match {
+        case TIntersection(type1, type2) =>
+            materials(context, type1) intersect materials(context, type2)
+        case TUnion(type1, type2) =>
+            materials(context, type1) union materials(context, type2)
+        case TProperty(property) =>
+            context.materials.filter(_._2.contains(property)).keySet
+        case TForget(_, _) =>
+            context.materials.flatMap(_._2).toSet
+    }
+
+    def forgotten(context: TypeContext, type0: Type) : Map[String, Map[String, Value]] = type0 match {
+        case TIntersection(type1, type2) =>
+            val materials1 = forgotten(context, type1)
+            val materials2 = forgotten(context, type2)
+            combineForgotten(materials1, materials2, materials1.keySet intersect materials2.keySet)
+        case TUnion(type1, type2) =>
+            val materials1 = forgotten(context, type1)
+            val materials2 = forgotten(context, type2)
+            combineForgotten(materials1, materials2, materials1.keySet union materials2.keySet)
+        case TProperty(property) =>
+            val materialNames = context.materials.filter(_._2.contains(property)).keys
+            materialNames.map(_ -> Map[String, Value]()).toMap
+        case TForget(property, value) =>
+            val forgetMap = Map(property -> value)
+            val emptyMap = Map[String, Value]()
+            context.materials.map { case (name, properties) =>
+                if(properties.contains(property)) name -> forgetMap
+                else name -> emptyMap
+            }
+    }
+
+    private def combineForgotten(
+        materials1: Map[String, Map[String, Value]],
+        materials2: Map[String, Map[String, Value]],
+        keys: Set[String]
+    ): Map[String, Map[String, Value]] = {
+        keys.map { key =>
+            val forgotten1 = materials1(key)
+            val forgotten2 = materials2(key)
+            val forgottenIntersection = forgotten1.keySet intersect forgotten2.keySet
+            forgottenIntersection foreach { k =>
+                if(forgotten1(k) != forgotten2(k)) {
+                    throw new RuntimeException(
+                        "Forgotten value mismatch: " + forgotten1(k) + " vs. " + forgotten2(k)
+                    )
+                }
+            }
+            key -> (forgotten1 ++ forgotten2)
+        }.toMap
+    }
+
+}
