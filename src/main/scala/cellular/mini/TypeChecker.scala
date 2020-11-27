2960c61afc6f55121adedf10750f5129832df6b5
ahnfelt <joakim.ahnfelt@gmail.com>
2020-11-27 18:00:52 +0100

    Experiments with new minimal core language

--- src/main/scala/cellular/mini/TypeChecker.scala
+++ src/main/scala/cellular/mini/TypeChecker.scala
@@ -2,28 +2,34 @@ package cellular.mini
 
 object TypeChecker {
 
-    def size(context: TypeContext, fixedType: FixedType) : Int = {
-        val materialNames = materials(context, fixedType.valueType).toList
-        materialNames.map { material =>
-            context.materials(material).map {
-                case MaterialProperty(_, Some(_)) => 1
-                case MaterialProperty(propertyName, None) if fixedType.fixed.exists(_.property == propertyName) => 1
-                case MaterialProperty(propertyName, None) =>
-                    context.properties(propertyName) match {
-                        case Some(fixedType1) => size(context, fixedType1)
-                        case None => 1
-                    }
-            }.product
-        }.sum
+    def size(context: TypeContext, fixed: FixedType): Int = {
+        val materialNames = materials(context, fixed.valueType).toList
+        materialNames.map(materialSize(context, _, fixed.fixed)).sum
     }
 
-    def materials(context: TypeContext, type0: Type) : Set[String] = type0 match {
+    def materialSize(context: TypeContext, materialName: String, fixed: List[PropertyValue] = List()): Int = {
+        context.materials(materialName).map {
+            case MaterialProperty(_, Some(_)) => 1
+            case MaterialProperty(propertyName, None) if fixed.exists(_.property == propertyName) => 1
+            case MaterialProperty(propertyName, None) =>
+                context.properties(propertyName) match {
+                    case Some(fixedType1) => size(context, fixedType1)
+                    case None => 1
+                }
+        }.product
+    }
+
+    def materials(context: TypeContext, type0: Type): Set[String] = type0 match {
         case TIntersection(type1, type2) =>
             materials(context, type1) intersect materials(context, type2)
         case TUnion(type1, type2) =>
             materials(context, type1) union materials(context, type2)
         case TProperty(property) =>
             context.propertyMaterials(property)
+    }
+
+    def convert(context: TypeContext, fixed1: FixedType, fixed2: FixedType): Int => Int = {
+        ???
     }
 
     def main(args : Array[String]) : Unit = {
