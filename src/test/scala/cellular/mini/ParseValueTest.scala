fba3221447b2958809452c8e7d18ea2f887b20a7
Michael Werk Ravnsmed <mfwerk@gmail.com>
2021-02-02 22:13:46 +0100

    Parse Value

---
+++ src/test/scala/cellular/mini/ParseValueTest.scala
@@ -0,0 +1,15 @@
+package cellular.mini
+
+object ParseValueTest {
+
+    def main(args : Array[String]) : Unit = {
+
+        val s1 = "Cave Foreground(Imp ImpStep(2) ImpClimb(None) DirectionH(Right) Content(None)) Background(None)"
+        val v = new Parser(s1).parseValue()
+        val s2 = v.toString
+        println(s1)
+        println(s2)
+        println(s1 == s2)
+    }
+
+}
