52ae8402695aaf04e2106ea5203d7c7fb04b39e4
Michael Ravnsmed <mfwerk@gmail.com>
2025-02-23 23:26:24 +0100

    wood, campfire

--- src/main/scala/cellular/frontend/InitialMap.scala
+++ src/main/scala/cellular/frontend/InitialMap.scala
@@ -14,6 +14,8 @@ class InitialMap(context : TypeContext, width : Int, height : Int) {
     private val rock = encode(read("Rock Vein(RockOre) Light(0) Dig(0)"))
     private val sand = encode(read("Cave Foreground(Sand) Background(None)"))
     private val water = encode(read("Cave Foreground(Water) Background(None)"))
+    private val wood = encode(read("Cave Foreground(Wood) Background(None)"))
+    private val campfire = encode(read("Building BuildingVariant(Campfire CampfireFuel(100))"))
 
     private def set(x : Int, y : Int, integer : Long) = {
         val i = (y * width) + x
@@ -31,6 +33,8 @@ class InitialMap(context : TypeContext, width : Int, height : Int) {
         case (5, 6) => box
         case (5, 3) => sand
         case (6, 3) => water
+        case (7, 3) => wood
+        case (8, 3) => campfire
         case (x, y) if 5 <= x && x <= 10 && 5 <= y && y <= 10 => cave
         case _ => rock
     }
